302fe5dee599b4a73fda7c52ed23361200cfdb48
AnNguyen199x <115407728+AnNguyen199x@users.noreply.github.com>
2023-09-30 03:50:01 +0000

    The example of creating a Script as a Module and Import from other

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+This is a module return the name of module
+"""
+def principal():
+    """
+        This is a function to return the module name
+    """
+    return f"The name of the module: {__name__}"
+    # code here
+    # anything ...
---
+++ other.py
@@ -0,0 +1,8 @@
+"""
+This is a Script example about import Module
+"""
+
+import main
+
+print(main.principal())
+print(f"The name of the file: {__name__}")
